5748cb90f841e8898eba02103a3e72343091c7b9
owenhankinson <owen@bookreport.io>
2023-03-29 19:29:48 -0700

    initial setup

--- back-end/api.py
+++ back-end/api.py
@@ -1,10 +1,14 @@
 import flask
+from car_detection import detectAndCountCars
+
 
 app = flask.Flask(__name__)
 
-@app.route('/status')
+@app.route('/') # on load of page
 def hello():
     return "Hello, World!"
+
+
 
 if __name__ == '__main__':
     app.run()
